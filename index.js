2377d1d9c15d39b10c334bca69ee1302ebfb3c2f
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-07-26 15:17:24 -0300

    chore: log request body

--- index.js
+++ index.js
@@ -42,8 +42,6 @@ app.post('/call-status-tracking', (req, res) => {
 app.post('/call', (req, res) => {
   const { id, from: caller } = req.body
 
-  console.log('req.body', req.body)
-
   if (!id || !caller) {
     return res.end(JSON.stringify({ status: 'error' }))
   }
@@ -69,6 +67,7 @@ app.post('/call', (req, res) => {
 
 app.post('/forward-to', (req, res) => {
   const call = req.body
+  console.log('req.body', req.body)
 
   postgresClient
     // TODO: pass to a postgraphql view
@@ -77,25 +76,28 @@ app.post('/forward-to', (req, res) => {
       const response = new VoiceResponse()
       console.log('row', row)
 
-      response.say(
-        { voice: 'alice', language: 'pt-BR' },
-        'Olá, eu sou a Alice uma inteligência artificial do BONDE. ' +
-        'Obrigada pela sua contribuição na nossa causa! ' +
-        'Espere só um instante, que vou te conectar com um dos alvos da mobilização.'
-      )
+      if (row) {
+        response.say(
+          { voice: 'alice', language: 'pt-BR' },
+          'Olá, eu sou a Alice uma inteligência artificial do BONDE. ' +
+          'Obrigada pela sua contribuição na nossa causa! ' +
+          'Espere só um instante, que vou te conectar com um dos alvos da mobilização.'
+        )
 
-      const dial = response.dial({ callerId: row.from })
-      row.to.split(',').forEach(to => {
-        dial.number({
-          statusCallbackEvent: 'initiated ringing answered completed',
-          statusCallback: `${process.env.APP_DOMAIN}/call-status-tracking`,
-          statusCallbackMethod: 'POST'
-        }, to)
-      })
-      // maybe a thank you message?
+        const dial = response.dial({ callerId: row.from })
+        row.to.split(',').forEach(to => {
+          dial.number({
+            statusCallbackEvent: 'initiated ringing answered completed',
+            statusCallback: `${process.env.APP_DOMAIN}/call-status-tracking`,
+            statusCallbackMethod: 'POST'
+          }, to)
+        })
+        // maybe a thank you message?
 
-      res.set('Content-Type', 'text/xml')
-      res.send(response.toString())
+        res.set('Content-Type', 'text/xml')
+        res.send(response.toString())
+      }
+      res.end(JSON.stringify({ status: 'ok' }))
     })
 })
 
@@ -122,7 +124,6 @@ postgresClient.connect(err => {
   postgresClient.on('notification', ({ channel, payload: textPayload }) => {
     if (channel === 'twilio_call_created') {
       const payload = JSON.parse(textPayload) || {}
-      console.log('payload', payload)
       request.post(`${process.env.APP_DOMAIN}/call`, { form: payload })
     }
   })
